4324b4d63eecedbbd69bc90b92979f95fb5d9b01
Ali Mansour <ali.a.mansour2004@gmail.com>
2025-11-07 23:58:06 +0200

    Initial Commit

---
+++ SwiftUIApi/SwiftUIApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiApp.swift
+//  SwiftUIApi
+//
+//  Created by Ali Mansour on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
